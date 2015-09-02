932edf35e4bf232b2cc0c1395d97e2530f4c6159
friot <cfriot@student.42.fr>
2015-09-02 17:50:45 +0200

    wall de mission fonctionnel i guess

--- packages/mission/public/hooks.js
+++ packages/mission/public/hooks.js
@@ -2,8 +2,4 @@ Mission.after.insert(function () {
   var post = {username: "Collectivz", content: 'content du post'};
   var wall = {key: this._id, from: "mission", posts: [post]};
   Walls.insert(wall);
-  wall = Walls.findOne({key: this._id});
-  Mission.update(this._id, {
-    $set: {wall: wall._id}
-  })
 });
