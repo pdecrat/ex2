9baa0f40351e34f3457ac7ceab267a76430da048
friot <cfriot@student.42.fr>
2015-09-02 22:05:15 +0200

    autorisations des post

--- packages/wall/public/methods.js
+++ packages/wall/public/methods.js
@@ -1,52 +1,26 @@
 Meteor.methods({
    insertPost: function(userId, wallId, doc) {
     wall = Wall.findOne({key: wallId});
-    if (wall.from === 'idea')
-    {
-      Wall.update(wall._id, {
-      $addToSet: {posts: doc},
-      });
-    }
-    else if (wall.from === 'project')
+    if (wall.from === 'project')
     {
       obj = Project.findOne(wall.key);
-      if (_.include(obj.members, userId))
-      {
-        Wall.update(wall._id, {
-        $addToSet: {posts: doc},
-        });
-      }
+      if (!_.include(obj.members, userId))
+          return false;
     }
     else if (wall.from === 'team')
     {
- //     obj = Teams.findOne(wall.key);
-   //   if (_.include(obj.members.key, userId))
- //     {
-        Wall.update(wall._id, {
-        $addToSet: {posts: doc},
-        });
- //     }
+      obj = Teams.findOne(wall.key);
+      if (!_.include(obj.members.key, userId))
+        return false;
     }
     else if (wall.from === 'mission')
     {
- //     obj = Teams.findOne(wall.key);
-   //   if (_.include(obj.members.key, userId))
- //     {
-        Wall.update(wall._id, {
-        $addToSet: {posts: doc},
-        });
- //     }
+      obj = Teams.findOne(wall.key);
+      if (!_.include(obj.members.key, userId))
+          return false;
     }
-    else
-      return false;
-//
- //   var ideas = Ideas.findOne(ideasId);
-   // if (_.include(ideas.members, this.userId))
-     // return false;
-//
-  //
-    //ideas = Ideas.findOne(ideasId);
-    //if (ideas.votes >= ideas.obj_backers)
-    //  Meteor.call('upgrated', ideasId);
-  }
+    Wall.update(wall._id, {
+      $addToSet: {posts: doc},
+    });
+    }
 });
