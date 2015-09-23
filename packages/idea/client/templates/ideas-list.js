9a2cb8f54261f2b4a5b221229e90fd1615d6a157
tommy durand <tdurand@student.42.fr>
2015-09-23 17:02:09 +0200

    search update

--- packages/idea/client/templates/ideas-list.js
+++ packages/idea/client/templates/ideas-list.js
@@ -1,5 +1,6 @@
 Template.ideaListDisplay.onCreated(function() {
 	var self = this;
+	self.search = new ReactiveVar(null);
 	self.autorun(function() {
 		var sub = self.subscribe('idea', {action: 'list'});
 	});
@@ -11,22 +12,26 @@ Template.ideaListDisplay.onCreated(function() {
 Template.ideaListDisplay.helpers({
 	ideas: function() {
 		return Template.instance().getIdeas();
+	},
+	search: function() {
+		if (Template.instance()) {
+			t = Template.instance().search.get();
+			if (t == "" || t == undefined || t == null)
+				return false;
+			return true;
+		}
+		return false
+	},
+	getIdeas: function() {
+		return IdeaSearch.getData();
 	}
 });
 
-Template.ideaSearch.helpers({
-  getIdeas: function() {
-    return IdeaSearch.getData();
-  }
-});
 
-Template.searchBox.events({
-  "keyup #search-box": _.throttle(function(e) {
-    var text = $(e.target).val().trim();
-    IdeaSearch.search(text);
-  }, 200)
-});
-
-Template.searchBox.onCreated(function() {
-	IdeaSearch.search("");
+Template.ideaListDisplay.events({
+  "keyup #search-box": _.throttle(function(e, t) {
+		template = t;
+			t.search.set($(e.target).val().trim());
+    	IdeaSearch.search(t.search.get());
+	  }, 200)
 });
